2433dc1299e57214abbaa661f76a3a6fc50f2fbb
Fortis Irdmousa <moosorkh@gmail.com>
2024-09-08 02:11:30 -0700

    Nav links Css added

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
         <div className='flex items-center justify-between w-[80%] mx-auto h-[100%]'>
         <h1 className='flex-[0.6] cusrsor-pointer text-[25px] text-white font-bold'>
             WEB
-            <span className='text-white'>DEV</span>
+            <span className='text-yellow-300'>DEV</span>
         </h1>
         <div className='nav-link'>Home</div>
         <div className='nav-link'>SERVICES</div>

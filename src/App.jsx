2582acd726222c0f0dfb1923d0d7ab6e69e1a706
Fortis Irdmousa <moosorkh@gmail.com>
2024-09-15 03:28:09 -0700

    finished About and Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import About from "./assets/Components/About/About";
+import Home from "./assets/Components/Home/Home";
 import Navbar from "./assets/Components/Navbar/Navbar";
 
 function App() {
   return <div className="bg-[#171d32] h-auto w-full overflow-hidden">
     <Navbar />
+    <Home />
+    <About />
   </div>;
 }
 

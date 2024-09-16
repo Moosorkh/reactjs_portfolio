e0d04ceaa5ef9eca96aeccc422af730c34ad5e5c
Fortis Irdmousa <moosorkh@gmail.com>
2024-09-15 18:59:03 -0700

    Projects and ProjectCard completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./assets/Components/About/About";
 import Experience from "./assets/Components/Experience/Experience";
 import Home from "./assets/Components/Home/Home";
 import Navbar from "./assets/Components/Navbar/Navbar";
+import Projects from "./assets/Components/Projects/Projects";
 
 function App() {
   return <div className="bg-[#171d32] h-auto w-full overflow-hidden">
@@ -9,6 +10,7 @@ function App() {
     <Home />
     <About />
     <Experience />
+    <Projects />
   </div>;
 }
 

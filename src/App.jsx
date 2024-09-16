2c9355c9cb708a4c5e0294d4f9eb17c8ee030e46
Fortis Irdmousa <moosorkh@gmail.com>
2024-09-15 17:28:41 -0700

    Experience cpmpleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./assets/Components/About/About";
+import Experience from "./assets/Components/Experience/Experience";
 import Home from "./assets/Components/Home/Home";
 import Navbar from "./assets/Components/Navbar/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <Navbar />
     <Home />
     <About />
+    <Experience />
   </div>;
 }
 

79a6113e2650234be8e7afdcf826d058a2630e10
Fortis Irdmousa <moosorkh@gmail.com>
2024-09-15 19:11:38 -0700

    Footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./assets/Components/About/About";
 import Experience from "./assets/Components/Experience/Experience";
+import Footer from "./assets/Components/Footer/Footer";
 import Home from "./assets/Components/Home/Home";
 import Navbar from "./assets/Components/Navbar/Navbar";
 import Projects from "./assets/Components/Projects/Projects";
@@ -11,6 +12,7 @@ function App() {
     <About />
     <Experience />
     <Projects />
+    <Footer />
   </div>;
 }
 
